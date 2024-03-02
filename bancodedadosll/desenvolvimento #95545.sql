ebccf24f27683576829a4099eec7df263ba68a1c
Hielen <149023509+Hielenmn@users.noreply.github.com>
2024-03-02 10:44:59 -0300

    Update desenvolvimento #95545.sql
    alguns ajustes

--- bancodedadosll/desenvolvimento #95545.sql
+++ bancodedadosll/desenvolvimento #95545.sql
@@ -1,11 +1,10 @@
--- Criar a tabela 'clientes' para fins de exemplo
+
 CREATE TABLE clientes (
   cliente_id INT PRIMARY KEY,
   nome VARCHAR(255),
   data_cadastro DATE
 );
 
--- Inserir dados de exemplo
 INSERT INTO clientes (cliente_id, nome, data_cadastro)
 VALUES
   (1, 'Cliente 1', '2024-03-01'),
@@ -13,7 +12,7 @@ VALUES
   (3, 'Cliente 3', '2024-03-02'),
   (4, 'Cliente 4', '2024-03-02');
 
--- Criar a função para somar clientes cadastrados em um dia
+
 DELIMITER //
 
 CREATE FUNCTION total_clientes_cadastrados(data_pesquisa DATE)
